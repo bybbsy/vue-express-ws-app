a0fb5d96b486dcb69e17b9e49620778651510fcf
Kirill Pulnev <kirill_pulnev@senla.eu>
2023-03-23 19:52:19 +0300

    какие-то изменения

--- react-client/src/components/Chat/MessageInput.tsx
+++ react-client/src/components/Chat/MessageInput.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import { Textarea } from '@chakra-ui/react';
 
+
+
 export function MessageInput () {
-  const [inputValue, setInputValue] = useState('');
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => setInputValue(e.target.value);
-
   return (
     <Textarea
       resize='none'
